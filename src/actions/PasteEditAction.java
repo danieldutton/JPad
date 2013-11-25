32f345cf84d830691b1ddf8f8919b232bcbf1c5d
Daniel Dutton <danielbdutton@googlemail.com>
2013-11-25 18:50:03 +0000

    Paste text action now working on menu and toolbar

--- src/actions/PasteEditAction.java
+++ src/actions/PasteEditAction.java
@@ -1,16 +1,20 @@
 package actions;
 
+import gui.components.GuiTextArea;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class PasteEditAction extends AbstractAction {
+
     private static final ImageIcon pasteEditIcon = new ImageIcon("pasteFile.png");
+    private JTextArea ta = GuiTextArea.getDefaultGuiTextArea();
 
     public PasteEditAction() {
         super("Paste", pasteEditIcon);
     }
 
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Yes I Work");
+        ta.paste();
     }
 }
