c60bd3de911e89123bc780555fc89cf88c36a364
Daniel Dutton <danielbdutton@googlemail.com>
2013-11-29 10:37:28 +0000

    Cut icon now visible on toolbar

--- src/gui/components/MenuBar.java
+++ src/gui/components/MenuBar.java
@@ -357,7 +357,6 @@ public class MenuBar extends JMenuBar {
                 TextArea.getDefaultGuiTextArea().setWrapStyleWord(true);
                 isWrapped = true;
             }
-
         }
     }
 
