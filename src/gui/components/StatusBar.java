9ec88763c203153b87952ae398c43e03a85f9db1
Daniel Dutton <danielbdutton@googlemail.com>
2013-12-10 17:18:23 +0000

    Status Bar Various
    RowCol count label positioned to the left
    padding added to label

--- src/gui/components/StatusBar.java
+++ src/gui/components/StatusBar.java
@@ -1,6 +1,7 @@
 package gui.components;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class StatusBar {
@@ -27,14 +28,15 @@ public class StatusBar {
 
      private static void initStatusBar(){
         statusBar.setOpaque(true);
+        statusBar.setLayout(new GridLayout());
         statusBar.add(lblRowColCount);
+        statusBar.setBorder(new EmptyBorder(2, 2, 2, 5));
      }
 
     private static void initRowColCountLabel()
     {
-        lblRowColCount.setHorizontalAlignment(SwingConstants.LEFT);
         lblRowColCount.setOpaque(true);
-        lblRowColCount.setFont(new Font("", 0, 10));
+        lblRowColCount.setFont(new Font("", 0, 12));
         lblRowColCount.setText("Line: 0" + " Column: 0");
     }
 
